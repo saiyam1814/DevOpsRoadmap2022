842216392ab2fa66d8eb7f5fb540b8d1879aee1c
Anubhav Vardhan <vardhananubhav@gmail.com>
2022-01-16 09:38:06 +0530

    Variables

--- Anubhav-Vardhan/Go/cards/main.go
+++ Anubhav-Vardhan/Go/cards/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var card string = "Ace of Spades"
+	// var card string = "Ace of Spades"
 	/*
 		var				=> We're about to declare a new variable
 		card			=> The name of the variable
@@ -12,5 +12,17 @@ func main() {
 		"Ace of Spades"	=> Assign the value "Ace of Spades" to card variable
 	*/
 
+	card := "Ace of Spades" //Alternative way to declare variable
+
 	fmt.Println(card)
+
+	/*
+		Go variable types
+		bool	=> true false
+		string	=> "hi" "How's it going"
+		int		=> 0 -10000 99999
+		float64	=> 10.000001 0.00009 -100.003
+
+		There are many more types but these are the fundamental
+	*/
 }
